8502119d858210718794186eb66fced095236be2
mufid219 <ardianto219@gmail.com>
2022-12-21 20:38:30 +0700

    membuat koneksi api dan state redux

---
+++ src/states/users/action.js
@@ -0,0 +1,20 @@
+import api from '../../utils/api';
+
+const ActionType = {
+  RECEIVE_USERS: 'RECEIVE_USERS',
+};
+
+const receiveUsersActionCreator = (users) => ({
+  type: ActionType.RECEIVE_USERS,
+  payload: { users },
+});
+
+const asyncRegisterUser = ({ name, email, password }) => async () => {
+  try {
+    await api.register({ name, email, password });
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+export { ActionType, receiveUsersActionCreator, asyncRegisterUser };
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,22 @@
 const api = (() => {
   const BASE_URL = 'https://forum-api.dicoding.dev/v1';
+
+  function putAccessToken(token) {
+    localStorage.setItem('accessToken', token);
+  }
+  function getAccessToken() {
+    localStorage.getItem('accessToken');
+  }
+  async function _fetchWithAuth(url, options = {}) {
+    return fetch(url, {
+      ...options,
+      headers: {
+        ...options.headers,
+        Authorization: `Bearer ${getAccessToken()}`,
+      },
+    });
+  }
+
   async function register({ name, email, password }) {
     const response = await fetch(`${BASE_URL}/register`, {
       method: 'POST',
@@ -19,8 +36,69 @@ const api = (() => {
     const { data: { user } } = responseJson;
     return user;
   }
+
+  async function login({ email, password }) {
+    const response = await fetch(`${BASE_URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+    const responseJson = await response.json();
+    const { status, message } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+    const { data: { token } } = responseJson;
+    return token;
+  }
+
+  async function getOwnProfile() {
+    const response = await _fetchWithAuth(`${BASE_URL}/users/me`);
+    const responseJson = await response.json();
+    const { status, message } = responseJson;
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+    const { data: { user } } = responseJson;
+    return user;
+  }
+
+  async function getAllUsers() {
+    const response = await fetch(`${BASE_URL}/users`);
+    const responseJson = await response.json();
+    const { status, message } = responseJson;
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+    const { data: { users } } = responseJson;
+    return users;
+  }
+
+  async function getAllThreads() {
+    const response = await fetch(`${BASE_URL}/threads`);
+    const responseJson = await response.json();
+    const { status, message } = responseJson;
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+    const { data: { theads } } = responseJson;
+    return theads;
+  }
+
   return {
     register,
+    putAccessToken,
+    getAccessToken,
+    login,
+    getOwnProfile,
+    getAllUsers,
+    getAllThreads,
   };
 })();
 
